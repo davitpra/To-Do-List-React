f53567171c2d4483ebfc73d2fc4e410c9aedd9f4
David <j.david.prado1716@gmail.com>
2022-08-10 20:58:39 -0500

    bug fixed on module resolve

---
+++ src/App/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { AppUI } from "./AppUI";
+
+function App() {
+    // Traemos nuestros TODOs almacenados
+    const localStorageTodos = localStorage.getItem('TODOS_V1');
+    let parsedTodos;
+
+    if (!localStorageTodos) {
+      // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
+        localStorage.setItem('TODOS_V1', JSON.stringify([]));
+        parsedTodos = [];
+    } else {
+      // Si existen TODOs en el localStorage los regresamos como nuestros todos
+        parsedTodos = JSON.parse(localStorageTodos);
+    }
+
+    // Guardamos nuestros TODOs del localStorage en nuestro estado
+    const [todos, setTodos] = React.useState(parsedTodos);
+    // Cantidad de TODOs completados
+    const completedTodos = todos.filter(todo => todo.completed).length;
+    // Cantidad total de TODOs
+    const totalTodos = todos.length;
+
+    // El estado de nuestra búsqueda
+    const [searchValue, setSearchValue] = React.useState('');
+    // Creamos una nueva variable en donde guardaremos las coincidencias con la búsqueda
+    let searchedTodos = [];
+    // Lógica para filtrar
+    if (!searchValue.length >= 1) {
+        searchedTodos = todos;
+    } else {
+        searchedTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+        return todoText.includes(searchText);
+        });
+    };
+    // Logica para cambiar las tareas a completado o descompletado
+    const toggleCompleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+        setTodos(newTodos);
+    };
+
+    // Logica para eliminar tareas de la lista 
+    const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos.splice(todoIndex, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+    <>
+    <AppUI
+        totalTodos={totalTodos}
+        completedTodos={completedTodos}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        searchedTodos={searchedTodos}
+        completeTodo={toggleCompleteTodo}
+        deleteTodo={deleteTodo}
+    />        
+    </>
+);
+}
+
+export default App;
---
+++ src/TodoCounter/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './TodoCounter.css';
+
+// Desestructuramos los props que pasamos al componente
+function TodoCounter({ total, completed }){
+    return(
+        <h2 className="TodoCounter"> Has complentado {completed} de {total} ToDos</h2>
+    )
+}
+
+export {TodoCounter};
