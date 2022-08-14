a72e2a5ce2ed1b5c12575d9380aa3a9c6a181ab4
David <j.david.prado1716@gmail.com>
2022-08-14 15:32:08 -0500

    added button submit function

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -4,7 +4,10 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import { TodoForm } from '../TodoForm';
 import { CreateTodoButton } from '../CreateTodoButton/index';
+import {Modal} from '../Modal'
+
 
 function AppUI() {
     const {
@@ -13,6 +16,8 @@ function AppUI() {
         searchedTodos,
         completeTodo,
         deleteTodo,
+        openModal,
+        setOpenModal,
     } = React.useContext (TodoContext)
 
     return (
@@ -24,6 +29,7 @@ function AppUI() {
             {error && <p>Oooops.... Hubo un error</p>}
             {loading && <p>Cargando...</p>}
             {(!loading && !searchedTodos.lengh) && <p>!Crea tu primer ToDo</p>}
+
             {searchedTodos.map(todo => (
                 <TodoItem
                     key={todo.text}
@@ -34,7 +40,16 @@ function AppUI() {
                 />
             ))}
         </TodoList>
-        <CreateTodoButton />
+
+        {!!openModal && (
+            <Modal>
+                <TodoForm/>
+            </Modal>
+        )}
+
+        <CreateTodoButton 
+            setOpenModal = {setOpenModal}
+        />
     </>
     );
 }
--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props){
+    const onClickButton =() => {
+        props.setOpenModal (prevState => !prevState);
+    }
     return(
         <button 
             className="CreateTodoButton"
+            onClick={onClickButton}
         >
         +
         </button>
