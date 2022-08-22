817a4900963a8eeb8e1ea38413f6d28595d47fe9
David <j.david.prado1716@gmail.com>
2022-08-22 14:21:42 -0500

    adding style into sincronize and debugging

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import './TodoList.css'
 
 function TodoList(props){
-    // const renderFunc = props.children || props.render;
+    
     return(
         <section className="TodoList-container">
+            {props.error && props.onError ()}
+            {props.loading && props. onLoading ()}
             {!props.loading && !props.totalTodos && props.onEmptyTodos()}
             {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
-            <ul>
-                {props.searchedTodos.map(props.render)}
-            </ul>
+            {(!props.loading && !props.error) && props.searchedTodos.map(props.render)}
+            
         </section>
     );
 }
